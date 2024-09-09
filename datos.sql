a1aab0362431788340be0a052b18b4645194be25
Manuel David Santos <fakemanuelfake@gmail.com>
2024-09-09 08:14:06 -0300

    Leon Kennedy added to the DB.

--- datos.sql
+++ datos.sql
@@ -36,13 +36,14 @@ VALUES
 ('Solid', 'Snake', 'DNI', 1003, 'Shadow Moses Island, 789', '555-555-5555', 13579246, '1974-07-07'),
 ('John', 'Marston', 'DNI', 1004, 'Rancho Beecher, 789', '555-111-2222', 98765432, '1873-05-09'),
 ('Michael', 'De Santa', 'DNI', 1005, 'Rockford Hills, 456', '555-333-4444', 65432109, '1965-17-02'),
-('Revolver', 'Ocelot', 'DNI', 1006, 'Shadow Moses Island, 123', '555-777-8888', 24681357, '1971-06-06'),
+('Revolver', 'Ocelot', 'DNI', 1006, 'Целиноярск, 123', '555-777-8888', 24681357, '1971-06-06'),
 ('Dutch', 'van der Linde', 'DNI', 1007, 'Horseshoe Overlook, 789', '555-999-0000', 31415926, '1858-15-11'),
 ('Franklin', 'Clinton', 'DNI', 1008, 'Strawberry, 456', '555-222-3333', 27182818, '1988-23-04'),
 ('Sniper', 'Wolf', 'DNI', 1009, 'Shadow Moses Island, 789', '555-444-5555', 88888888, '1970-24-12'),
 ('William Joseph', 'Blazkowicz', 'DNI', 1010, 'Mesquite, 255', '555-555-6666', 77777777, '1885-12-03'),
 ('Lester','Crest','DNI',1011,'Murrieta Heights, 2167','324-789-2266',23412909,'1960-12-02')
 ('Cloude','Strife','DNI',1012,'Nibelheim village, 147','345-999-2218',34829219,'1986-11-08')
+('Leon','Kennedy','DNI',2101,'Raccoon City, 1998','4-960-000',21011998,'1977-09-9')
 
 
 
